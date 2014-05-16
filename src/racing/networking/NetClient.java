f5b5923f12280aecb3d2bcf048d1432dd90f78e2
James Maciel <james3015@gmail.com>
2014-05-16 12:45:26 -0400

    fixing other stuff

--- src/racing/networking/NetClient.java
+++ src/racing/networking/NetClient.java
@@ -4,7 +4,6 @@ import java.net.*;
 public class NetClient {
 	private Socket socket;
 	private ObjectInputStream input;
-	private BufferedReader usrinput;
 	private PrintWriter output;
 	/**
 	 * 
@@ -14,7 +13,6 @@ public class NetClient {
 	public NetClient(InetAddress address, int port){
 		try {
 			socket=new Socket(address,port);
-			usrinput=new BufferedReader(new InputStreamReader(System.in));
 			input=new ObjectInputStream(socket.getInputStream());
 			output=new PrintWriter(socket.getOutputStream());
 			
@@ -23,17 +21,7 @@ public class NetClient {
 		}
 	}
 	public void run() throws IOException{
-		String input;
-		while(usrinput.readLine()!=null){
-			try {
-//				input=usrinput.readLine();
-				output.print(2);
-//				System.out.println("Sent: "+input);
-				System.out.println("Received: "+this.input.readObject().toString());
-			} catch (IOException | ClassNotFoundException e) {
-				System.out.println("IO Error: "+e.getMessage());
-			}
-		}
+		
 	}
 	public static void main(String[] args){
 		try {
