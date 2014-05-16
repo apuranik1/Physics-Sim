9d3feb16b46e6ccdcaa27955ea42a209d8811a3d
James Maciel <james3015@gmail.com>
2014-05-16 12:45:26 -0400

    fixing other stuff

--- src/racing/networking/NetServer.java
+++ src/racing/networking/NetServer.java
@@ -4,53 +4,57 @@ import java.io.IOException;
 import java.net.*;
 public class NetServer {
 	/**
-	 * Sockets clients are connected to
+	 * Server Threads clients are connected to
 	 */
-	private ArrayList<Socket> clients;
+	private ArrayList<NetServerThread> clients;
 	/**
 	 * Server socket
 	 */
 	private ServerSocket server;
 	/**
 	 * @param port Port to listen on
-	 * @param clients Number of clients to connect
 	 */
-	public NetServer(int port, int clients){
+	public NetServer(int port){
 		try {
-			server=new ServerSocket(port, 0, Inet4Address.getLocalHost());
-		} catch (UnknownHostException e) {
-			System.out.println("IP error");
+			server=new ServerSocket(port);
 		}
 		catch(IOException e){
 			System.out.println("Socket error");
 		}
-		this.clients=new ArrayList<Socket>(clients);
-		connect(clients);
+		this.clients=new ArrayList<NetServerThread>();
+	}
+	/**
+	 * @return Local IP Address
+	 */
+	public InetAddress getIP(){
+		try {
+			return Inet4Address.getLocalHost();
+		} catch (UnknownHostException e) {
+			System.out.println("IP error: "+e.getMessage());
+		}
+		return null;
 	}
 	/**
 	 * Connect clients
 	 * @param clients Number of clients to connect
 	 */
-	private void connect(int clients){
-		for(int i=0;i<clients;i++){
-			try {
-				this.clients.add(server.accept());
-				System.out.println("Connected");
-			} catch (IOException e) {
-				System.out.println("Connection error: "+e.getMessage());
-			}
+	public void connect(){
+		try {
+			this.clients.add(new NetServerThread(server.accept()));
+			System.out.println("Connected");
+			
+		} catch (IOException e) {
+			System.out.println("Connection error: "+e.getMessage());
 		}
 	}
 	/**
 	 * Start all threads
 	 */
 	public void start(){
-		for(Socket socket:clients){
-			NetServerThread thread=new NetServerThread(socket);
+		for(NetServerThread thread:clients)
 			thread.start();
-		}
 	}
 	public static void main(String[] args){
-		new NetServer(1337,2).start();
+		new NetServer(1337).start();
 	}
 }
