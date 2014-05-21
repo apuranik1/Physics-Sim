02b7485aa3b97c983e5da8858f5f2c4829c27918
James Maciel <james3015@gmail.com>
2014-05-21 08:21:08 -0400

    mostly

--- src/racing/networking/NetClient.java
+++ src/racing/networking/NetClient.java
@@ -1,31 +1,36 @@
 package racing.networking;
 import java.io.*;
 import java.net.*;
+
 import racing.game.Cart;
+import racing.game.Track;
 public class NetClient {
 	private Socket socket;
 	private Cart cart;
+	private Track track;
 	private ObjectInputStream input;
 	private PrintWriter output;
 	/**
-	 * 
 	 * @param address Address of server to connect to
 	 * @param port	Port of server to connect to
 	 * @param cart Client cart to update
+	 * @param Track Client track to update
 	 */
-	public NetClient(InetAddress address, int port, Cart cart){
+	public NetClient(InetAddress address, int port, Cart cart, Track track){
 		try {
 			socket=new Socket(address,port);
 			input=new ObjectInputStream(socket.getInputStream());
 			output=new PrintWriter(socket.getOutputStream());
 			this.cart=cart;
+			this.track=track;
 			
 		} catch (IOException e) {
 			System.out.println("IO error: "+e.getMessage());
 		}
 	}
-	public void run() throws IOException{
+	public void update() throws IOException{
 		output.print(cart);//send cart data
+		output.print(track.getItems());//send item data
 		try {
 			NetData data=(NetData)input.readObject();//receive server data
 		} catch (ClassNotFoundException e) {
@@ -33,6 +38,11 @@ public class NetClient {
 		}
 	}
 	public static void main(String[] args){
-		
+		NetClient client=null;
+		try {
+			client=new NetClient(InetAddress.getLocalHost(),5555,new Cart(),new Track());
+		} catch (UnknownHostException e) {
+			System.out.println("IP error: "+e.getMessage());
+		}
 	}
 }
