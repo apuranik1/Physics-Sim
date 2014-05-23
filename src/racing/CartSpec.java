ec0574d301544a205c99d5059b3ddd13ad476484
Alok <alokpuranik1@gmail.com>
2014-05-23 18:22:12 -0400

    Basic Cart framework

---
+++ src/racing/CartSpec.java
@@ -0,0 +1,13 @@
+package racing;
+
+public class CartSpec {
+
+	public double engineForce;
+	public int handling;
+
+	public CartSpec(double engineForce, int handling) {
+		this.engineForce = engineForce;
+		this.handling = handling;
+	}
+
+}
