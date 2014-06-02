2958b79a5515cc8d70134fe5f0b20b0e7b418afc
Alok <alokpuranik1@gmail.com>
2014-06-02 07:59:31 -0400

    Dummy constructor for Cart to shut up eclipse

--- src/racing/Cart.java
+++ src/racing/Cart.java
@@ -1,10 +1,21 @@
 package racing;
 
+import java.awt.Color;
+
 import engine.BoundingBox;
 import engine.graphics.Object3D;
+import engine.physics.Motion;
+import engine.physics.Vector2D;
+import engine.physics.Vector3D;
 
 public class Cart extends Object3D {
 	
+	public Cart(Vector3D[] vertices, Vector3D[] normals,
+			Vector2D[] textureCoords, Color[] colors, Motion motion) {
+		super(vertices, normals, textureCoords, colors, motion);
+		// TODO Auto-generated constructor stub
+	}
+
 	private BoundingBox bb;
 	
 	
