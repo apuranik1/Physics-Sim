d5ab7292546f3dd7c31b5edef5425cb73ac07ebe
James Maciel <james3015@gmail.com>
2014-06-05 11:18:49 -0400

    manager

--- src/racing/game/Manager.java
+++ src/racing/game/Manager.java
@@ -1,35 +1,91 @@
 package racing.game;
+import java.io.IOException;
 import java.net.InetAddress;
+import engine.GameEngine;
+import engine.ResourceManager;
+import engine.animation.Animator;
+import engine.animation.CameraFollow;
+import engine.physics.PhysicsSpec;
+import engine.physics.Vector3D;
+import racing.CarController;
+import racing.Cart;
 import racing.networking.NetClient;
 import racing.networking.NetServer;
 public class Manager{
-	public void initGame(){
-		
-	}
+	private static Manager manager;
+	private Cart cart;
+	private Track track;
+	private NetClient client;
+	private NetServer server;
 	/**
-	 * Create new singleplayer game
+	 * 
+	 * @param cartType Type of cart to create
+	 * @return Game Manager
 	 */
-	public void newSingle(){
-		
+	public static Manager initGame(int cartType) throws Exception{
+		if(manager!=null)return manager;
+		switch(cartType){
+		case 0:
+			manager=new Manager("",200);
+		}
+		return manager;
 	}
 	/**
-	 * Create new multiplayer game server
+	 * 
+	 * @param cartOBJ Path to cart object
+	 * @param cartMass Mass of cart
+	 */
+	private Manager(String cartOBJ, double cartMass) throws Exception{
+		GameEngine engine = GameEngine.getGameEngine();
+		ResourceManager rmanager = ResourceManager.getResourceManager();
+		cart=new Cart(cartOBJ);
+		cart.setAcceleration(Vector3D.origin);
+		cart.setSpec(new PhysicsSpec(false,false,true,cartMass));
+		rmanager.loadObject("cart0", cart);
+		cart=(Cart) rmanager.retrieveInstance(rmanager.insertInstance("cart0"));
+		
+		track=new Track();
+		
+		engine.beginGame();
+		engine.registerProcessor(new CarController(cart));
+		Animator.getAnimator().registerEvent(new CameraFollow(cart));
+	}
+	/**
+	 * Setup game server
 	 * @param port Port to host on
 	 * @return Server IP
 	 */
-	public NetServer newMultiServer(int port){
-		NetServer server=new NetServer(port);
-		NetClient client=newMultiClient(server.getIP(), port);//local client
+	public InetAddress setupServer(int port){
+		server=new NetServer(port);
+		client=new NetClient(server.getIP(), port,cart,track);//local client
 		server.connect();//connect local client
-		return server;
+		return server.getIP();
 	}
 	/**
-	 * Create new multiplayer player game client
+	 * Setup game client
 	 * @param address IP Address to connect to
 	 * @param port Port to connect to
 	 */
-	public NetClient newMultiClient(InetAddress address, int port){
-		NetClient client=new NetClient(address, port, cart, track);//remote client
+	public void setupClient(InetAddress address, int port){
+		client=new NetClient(address, port, cart, track);//remote client
+	}
+	public Cart getCart(){
+		return cart;
+	}
+	public Track getTrack(){
+		return track;
+	}
+	public NetServer getServer(){
+		return server;
+	}
+	public NetClient getClient(){
 		return client;
+	}
+	public static void main(String[] args){
+		try {
+			Manager.initGame(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
