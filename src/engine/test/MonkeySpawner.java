cde79995565dbfab4dd85682fd968e916939de8e
Alok <alokpuranik1@gmail.com>
2014-06-06 22:46:22 -0400

    I don't really know

--- src/engine/test/MonkeySpawner.java
+++ src/engine/test/MonkeySpawner.java
@@ -19,11 +19,11 @@ public class MonkeySpawner {
 		GameEngine engine = GameEngine.getGameEngine();
 		ResourceManager manager = ResourceManager.getResourceManager();
 
-		Object3D monkey = new Object3D("C:/Users/16apuranik/Downloads/objects/monkey.obj");
+		Object3D monkey = new Object3D("/run/media/root/Data/Downloads/monkey.obj");
 		// monkey.setAcceleration(new Vector3D(0,0,0));
 		monkey.setSpec(new PhysicsSpec(false, false, false, 25));
 		monkey.setRotation(new Quaternion(new Vector3D(1, 0, 0), Math.PI / 2));
-		Object3D floor = new Object3D("C:/Users/16apuranik/Downloads/objects/floor.obj");
+		Object3D floor = new Object3D("/run/media/root/Data/Downloads/floor.obj");
 		floor.setAcceleration(Vector3D.origin);
 		floor.setSpec(new PhysicsSpec(false, false, false, 1000000000000.0));
 		manager.loadObject("monkey", monkey);
@@ -36,7 +36,7 @@ public class MonkeySpawner {
 
 		// manager.insertInstance("monkey", new Vector3D(0, -10, 0));
 		final long floorl = manager.insertInstance("floor", new Vector3D(-5,
-				-10, -5));
+				-20, -5));
 
 		Animator.getAnimator().registerEvent(
 				new ContinuousAnimationEvent(0, .5) {
--- src/racing/CarDemo.java
+++ src/racing/CarDemo.java
@@ -15,20 +15,27 @@ public class CarDemo {
 		GameEngine engine = GameEngine.getGameEngine();
 		ResourceManager manager = ResourceManager.getResourceManager();
 		
-		Cart cart = new Cart("C:/Users/16apuranik/Downloads/objects/monkey.obj");
+		Cart cart = new Cart("/run/media/root/Data/Downloads/monkey.obj");
 		cart.setAcceleration(Vector3D.origin);
 		cart.setRotation(new Quaternion(new Vector3D(0,1,0), 3.041592653589793)); // this screws everything up
 		cart.setSpec(new PhysicsSpec(false, false, false, 25));
 		manager.loadObject("cart0", cart);
 		Cart myCart = (Cart) manager.retrieveInstance(manager.insertInstance("cart0", Vector3D.origin));
 		
-		Object3D floor = new Object3D("C:/Users/16apuranik/Downloads/objects/floor.obj");
+		Object3D floor = new Object3D("/run/media/root/Data/Downloads/floor.obj");
 		floor.setAcceleration(Vector3D.origin);
 		floor.setSpec(new PhysicsSpec(false, false, false, Double.POSITIVE_INFINITY));
-		floor.setRotation(new Quaternion(new Vector3D(1,0,0), Math.PI / 15));
+		floor.setRotation(new Quaternion(new Vector3D(1,0,0), Math.PI / 12));
 		manager.loadObject("floor", floor);
 		long l = manager.insertInstance("floor", new Vector3D(-5, -10, -5));
-		BoundingBox bb = manager.retrieveInstance(l).getBoundingBox().simpleBound();
+		floor.setRotation(new Quaternion(new Vector3D(1,0,0), -Math.PI / 12));
+		manager.insertInstance("floor", new Vector3D(-5, -15, -30));
+		
+		Object3D monkey = new Object3D("/run/media/root/Data/Downloads/monkey.obj");
+		monkey.setAcceleration(Vector3D.origin);
+		monkey.setSpec(new PhysicsSpec(false, false, false, 1e20));
+		manager.loadObject("block", monkey);
+		manager.insertInstance("block", new Vector3D(-1, -9.7, -1));
 
 		engine.beginGame();
 		
