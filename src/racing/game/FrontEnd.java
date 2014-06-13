f66be8fe687b3d48a4fdc176d46d90396c657896
James Maciel <james3015@gmail.com>
2014-06-12 21:31:53 -0400

    more frontend stuff

--- src/racing/game/FrontEnd.java
+++ src/racing/game/FrontEnd.java
@@ -5,15 +5,25 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.TextArea;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.swing.JLabel;
+
 import com.sun.awt.AWTUtilities;
+
 import racing.BasicGame;
 import engine.GameEngine;
 public class FrontEnd {
@@ -27,24 +37,25 @@ public class FrontEnd {
 	private FrontEnd(){
 		frame=new Frame("Racing");
 		frame.setLayout(new GridLayout(0,1));
+		frame.setPreferredSize(new Dimension(190,140));
 		((GridLayout) (frame.getLayout())).setHgap(5);
 		((GridLayout) (frame.getLayout())).setVgap(5);
 		Button newGame=new Button("New Game");
 		newGame.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
-				try {
-					new BasicGame();
-					GameEngine.getGameEngine().beginGame();
-					showPopup("Test Popup");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				typeSelect();
 			}
 		});
 		Button instructions=new Button("Instructions");
 		instructions.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				showInstructions();
+			}
+		});
+		Button quit=new Button("Quit");
+		quit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
 			}
 		});
 		//handle quit button
@@ -55,17 +66,17 @@ public class FrontEnd {
 		});
 		frame.add(newGame);
 		frame.add(instructions);
+		frame.add(quit);
 		frame.pack();
 		//Center frame
 		frame.setLocation(frame.getToolkit().getScreenSize().width/2-frame.getWidth()/2, frame.getToolkit().getScreenSize().height/2-frame.getHeight()/2);
 		frame.setVisible(true);
-		showPopup("Test Popup");
 	}
 	public void showPopup(String msg){
 		final Frame pframe=new Frame();
 		pframe.setUndecorated(true);
 		JLabel label=new JLabel(msg);
-		label.setFont(new Font("sansserif",Font.PLAIN,25));
+		label.setFont(new Font("sansserif",Font.BOLD,30));
 		AWTUtilities.setWindowOpaque(pframe, false);
 		pframe.add(label);
 		label.addMouseListener(new MouseListener(){
@@ -76,7 +87,7 @@ public class FrontEnd {
 			public void mouseExited(MouseEvent arg0) {
 			}
 			public void mousePressed(MouseEvent e) {
-				if(e.getButton()==MouseEvent.BUTTON1)pframe.setVisible(false);
+				if(e.getButton()==MouseEvent.BUTTON1)pframe.dispose();
 			}
 			public void mouseReleased(MouseEvent arg0) {
 			}
@@ -85,13 +96,94 @@ public class FrontEnd {
 		pframe.setLocation(pframe.getToolkit().getScreenSize().width/2-pframe.getWidth()/2, pframe.getToolkit().getScreenSize().height/2-pframe.getHeight()/2);
 		pframe.setVisible(true);
 	}
+	private void typeSelect(){
+		final Frame iframe=new Frame("Game Type Select");
+		iframe.setLayout(new GridLayout(0,1));
+		iframe.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				iframe.dispose();
+			}
+		});
+		((GridLayout) (iframe.getLayout())).setHgap(5);
+		((GridLayout) (iframe.getLayout())).setVgap(5);
+		Button single=new Button("Singleplayer");
+		single.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				try {
+					new BasicGame();
+				} catch (IOException e1) {
+					showPopup("Error: "+e1.getMessage());
+				}
+				GameEngine.getGameEngine().beginGame();
+				iframe.dispose();
+			}
+		});
+		Button multiServer=new Button("Multiplayer Server");
+		multiServer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		Button multiClient=new Button("Multiplayer Client");
+		multiClient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getIP();
+				iframe.dispose();
+			}
+		});
+		iframe.add(single);
+		iframe.add(multiServer);
+		iframe.add(multiClient);
+		iframe.pack();
+		iframe.setLocation(iframe.getToolkit().getScreenSize().width/2-iframe.getWidth()/2, iframe.getToolkit().getScreenSize().height/2-iframe.getHeight()/2);
+		iframe.setVisible(true);
+	}
+	private void getIP(){
+		final Frame iframe=new Frame("IP Address");
+		iframe.setLayout(new GridLayout(1,0));
+		iframe.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				iframe.dispose();
+			}
+		});
+		((GridLayout) (iframe.getLayout())).setHgap(5);
+		((GridLayout) (iframe.getLayout())).setVgap(5);
+		iframe.add(new Label("IP Address: "));
+		final TextField field=new TextField("");
+		field.setColumns(15);
+		iframe.add(field);
+		final Button enter=new Button("Enter");
+		enter.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				try {
+					InetAddress.getByName(field.getText());
+					iframe.dispose();
+				} catch (UnknownHostException e1) {
+					showPopup("Error: "+e1.getMessage());
+				}
+			}
+		});
+		field.addKeyListener(new KeyListener(){
+			public void keyTyped(KeyEvent e) {
+			}
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER)enter.dispatchEvent(new ActionEvent((Object)enter,ActionEvent.ACTION_PERFORMED,""));
+			}
+			public void keyReleased(KeyEvent e) {
+			}
+		});
+		iframe.add(enter);
+		iframe.pack();
+		iframe.setLocation(iframe.getToolkit().getScreenSize().width/2-iframe.getWidth()/2, iframe.getToolkit().getScreenSize().height/2-iframe.getHeight()/2);
+		iframe.setVisible(true);
+	}
 	private void showInstructions(){
 		final Frame iframe=new Frame("Instructions");
 		iframe.setLayout(new FlowLayout());
 		Button cancel=new Button("Cancel");
 		cancel.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
-				iframe.setVisible(false);
+				iframe.dispose();
 			}
 		});
 		TextArea ta=new TextArea(INSTRUCTIONS);
@@ -100,7 +192,7 @@ public class FrontEnd {
 		//handle quit button
 		iframe.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent windowEvent){
-	            iframe.setVisible(false);
+				iframe.dispose();
 			}
 		});
 		iframe.add(ta);
