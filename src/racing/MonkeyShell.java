9363d37b3716273d4845f779288f8a18d0d2f069
Alok <alokpuranik1@gmail.com>
2014-06-17 19:37:57 -0400

    Monkey shell fix 2

--- src/racing/MonkeyShell.java
+++ src/racing/MonkeyShell.java
@@ -20,7 +20,7 @@ public class MonkeyShell extends SyncableObject3D {
 	}
 
 	public void specialCollide(Object3D other) {
-		if (other instanceof TrackFloor)
+		if (other instanceof TrackFloor || !other.getSpec().isCollidable())
 			return;
 		collisionCount++;
 		if (collisionCount > 5)
@@ -44,7 +44,7 @@ public class MonkeyShell extends SyncableObject3D {
 		Object3D shell = rm.retrieveInstance(rm.insertInstance("monkey_shell",
 				position));
 		shell.setRotation(direction);
-		shell.setVelocity(direction.toMatrix().multiply(new Vector3D(0, 0, 20)));
+		shell.setVelocity(direction.toMatrix().multiply(new Vector3D(0, 0, 120)));
 	}
 
 	public MonkeyShell clone() {
